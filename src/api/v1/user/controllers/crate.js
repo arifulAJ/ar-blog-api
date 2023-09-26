244e6540dca68954a37100ef545f90aa092e41ba
Ariful islam <arifulislam10111999@gmail.com>
2023-09-26 10:20:47 +0600

    create users

--- src/api/v1/user/controllers/crate.js
+++ src/api/v1/user/controllers/crate.js
@@ -1,18 +1,38 @@
 const User = require("../../../../model/User");
+const { validationResult } = require("express-validator");
+const errorFormate = require("../../../../utils/validatorerrorFormator");
+const bcrypt = require("bcryptjs");
 
-exports.createuserController = async (req, res) => {
+/**
+ * create new user with minimum validation
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @returns
+ */
+exports.createuserController = async (req, res, next) => {
   if (!req.user) {
     return res.status(404).json({ message: "You need to sign in" });
   }
+  let errors = validationResult(req).formatWith(errorFormate);
 
+  if (!errors.isEmpty()) {
+    let data = errors.mapped();
+
+    return res.status(409).json({
+      code: 400,
+      message: "bad request",
+      data,
+    });
+  }
   try {
     const { name, email, password, avatar } = req.body;
-
+    let hashPassword = await bcrypt.hash(password, 11);
     // Create a new user using the User model (assuming it's properly defined)
     const newUser = new User({
       name,
       email,
-      password,
+      password: hashPassword,
       avatar,
     });
 
@@ -24,5 +44,6 @@ exports.createuserController = async (req, res) => {
       .json({ message: "User created successfully", user: savedUser });
   } catch (e) {
     console.log(e);
+    next(e);
   }
 };
--- src/routes/user/userRoute.js
+++ src/routes/user/userRoute.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const { findUsers } = require("../../api/v1/user/controllers/find");
-// const { createuserController } = require("../../api/v1/user/controllers/crate");
+const { createuserController } = require("../../api/v1/user/controllers/crate");
+const signupvalidator = require("../../../validator/auth/signupvalidator");
 
 /**
  * user all route are handes here
  */
 router.get("/user", findUsers);
-// router.post("/user", createuserController);
+router.post("/user", signupvalidator, createuserController);
 module.exports = router;
